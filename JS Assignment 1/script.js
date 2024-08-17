bdfc9a2afedba6907755454859939eca1ed3067e
IrfanRaza101 <alaxjoun31@gmail.com>
2024-08-17 22:49:57 +0500

    Add files via upload

---
+++ JS Assignment 1/script.js
@@ -0,0 +1,33 @@
+
+// ALERT
+
+
+var name1 = ('this is my first alert button')
+alert( name1);
+
+var email = "irfanraz67@gmail.com"
+alert("My email address is" +' '+ email)
+
+var book = "A smarter way to learn javascriipt"
+alert("I am trying to learn from the book"+' '+book)
+
+
+
+
+// VARIABLE FOR STRING
+
+var my_Name = ('My name is Irfan raza')
+document.write(my_Name);
+
+var $name1_ = ('this is my first text print')
+// document.write($name1_);
+
+
+
+
+// variable for number
+
+var num1= 7
+var num2= 3
+// document.write( num1 + num2)
+console.log( num1 + num2);
